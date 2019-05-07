6ee050d5434462bff25cdd9149776a055e78c6dc
GarginYT <50383963+GarginYT@users.noreply.github.com>
2019-05-07 17:02:42 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js');
+const logger = require('winston');
+const { prefix, token } = require('./auth.json');
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console,
+{
+    colorize: true
+    });
+
+logger.leve = 'debug';
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    logger.info('Gargitron activated.');
+});
+
+
+
+client.on('message', message => {
+    // If the message is in the roles channel
+    if (message.channel.id == '575068038387335198') {
+        // If the message is !lfg
+        if (message.content == "!lfg")
+        {
+            let role = message.member.guild.roles.get('575102690393063424');
+            message.member.addRole(role);
+        }
+        // If the message is !updates
+        if (message.content == "!updates")
+        {
+            let role = message.member.guild.roles.get('575102622776950804');
+            message.member.addRole(role);
+        }
+        // If the message is !video
+        if (message.content == "!video") {
+            let role = message.member.guild.roles.get('575067316753137674');
+            message.member.addRole(role);
+        } else if 
+        // If the message is !remove
+        if (message.content == "!remove")
+        {
+            message.member.removeRoles(['575102690393063424', '575102622776950804', '575067316753137674']);
+        }
+        message.delete(100);
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
